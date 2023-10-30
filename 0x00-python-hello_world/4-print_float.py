ccf7b2fb8257e007fcf5555b534ea73c296d143b
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-10-30 18:04:27 +0300

    modified the operators

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-print(f"Float: {number:.2f}") #f-string,expression evaluated at a runtime,format specification of the float number
+print(f"Float: {number:.2f}") #f-string,expression evaluated at a runtime,format specification of the float
