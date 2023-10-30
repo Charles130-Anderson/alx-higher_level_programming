fd83a0dbd536ba6af91ae52844ac2f13d56ea3a3
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-10-30 17:04:52 +0300

    print function,the f-string

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,3 @@
-#number holds the integer to be printed
-number = 98
-#use of f-string and print function
-print(f"{number} Battery street")
+#!/usr/bin/python3
+number = 98 #number holds the integer to be printed
+print(f"{number} Battery street") #use of f-string and print function
