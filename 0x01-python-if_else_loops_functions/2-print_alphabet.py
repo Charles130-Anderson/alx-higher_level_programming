2c59f2e3d497e09acdb7d37950bef51bd9d1e58d
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-01 07:45:58 -0700

    a program that prints the ASCII alphabet, in lowercase, not followed by a new line.

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for Anderson in range(97, 122 + 1):  # range of two numbers stored in Anderson
+    print("{}".format(chr(Anderson)), end="")  # printing that particular range
+    # char function changes number to ASCII code
