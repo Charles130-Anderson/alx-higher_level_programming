96d7645e75be102bc5f24df9d2f56b509d9b4822
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-01 10:08:56 -0700

    printing thre numbers in ascending order formating with 02 ,seperated by a comma,in a range .Finally the last digit ends in a new line.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,5 @@ for numbers_ascending in range(0, 97 + 3):
     if numbers_ascending == 99:
         print("{}".format(numbers_ascending))
     else:
-        print("{:02}".format(numbers_ascending), end=",")
+        print("{:02}".format(numbers_ascending), end=", ")
     #  Ascending order formating with 02 ,seperated by a comma,in a range.
