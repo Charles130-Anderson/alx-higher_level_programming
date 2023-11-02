7f805a6637bb356ae945a4691d4740660b6baece
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-02 11:55:37 -0700

    This code is a Python calculator that accepts command-line input for two numbers and an operator (+, -, *, /). It calculates the result of the specified operation and displays it, providing error messages for invalid input.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+
 from sys import argv
 from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     User_input = argv[1:]
     Count = len(User_input)
-    operator=  ["+","-","*","/"]
+    operator = ["+", "-", "*", "/"]
+
     if Count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     elif argv[2] not in operator:
         print("Available operators: +, -, * and /")
     else:
-        if  argv[2] == operator[0]:
+        if argv[2] == operator[0]:
             a = int(User_input[0])
             b = int(User_input[2])
-            print("{} + {} = {}".format(a,b, add(a,b)))
+            print("{} + {} = {}".format(a, b, add(a, b)))
         elif argv[2] == operator[1]:
             a = int(User_input[0])
             b = int(User_input[2])
-            print("{} - {} = {}".format(a,b, sub(a,b)))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         elif argv[2] == operator[2]:
             a = int(User_input[0])
             b = int(User_input[2])
-            print("{} * {} = {}".format(a,b, mul(a,b)))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         else:
             a = int(User_input[0])
             b = int(User_input[2])
-            print("{} / {} = {}".format(a,b, div(a,b)))
+            print("{} / {} = {}".format(a, b, div(a, b)))
