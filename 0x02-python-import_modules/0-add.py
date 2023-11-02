5b16429cdec06cbb3e894551a3944bbcac38db41
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-02 09:16:32 -0700

    The code, when executed directly, imports the 'add' function from the 'add_0' module, defines two variables 'a' and 'b,' and then performs addition using the 'add' function. It prints the equation and the result of adding 'a' and 'b.'

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-# Check if the script is being executed directly
 if __name__ == "__main__":
-    # Import the 'add' function from the 'add_0' module
     from add_0 import add
-
-    # Define two variables 'a' and 'b' with initial values
     a = 1
     b = 2
     a = int(a)
     b = int(b)
-    # Perform addition using the 'add' function and print the result
     print("{} + {} = {}".format(a, b, add(a, b)))
