9e142fcdfbbf9a17cb430138f22e76f0581aa639
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-02 05:59:10 -0700

    The code calculates and prints the number of command-line arguments and their values, indicating whether it's a single argument or multiple arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@
 if __name__ == "__main__":
     import sys
     length_argument = len(sys.argv)
-    
+
     if length_argument == 1:
         print("{} arguments.".format(length_argument - 1))
     elif length_argument == 2:
-        print("{} arguments.".format(length_argument - 1))
+        print("{} argument.".format(length_argument - 1))
     else:
         print("{} arguments.".format(length_argument - 1))
-    
+
     for y in range(1, length_argument):
-        print("{}: {}.".format(y, sys.argv[y]))
+        print("{}: {}".format(y, sys.argv[y]))
