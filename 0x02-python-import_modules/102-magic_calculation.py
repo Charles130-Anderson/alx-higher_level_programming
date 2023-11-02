1b72b12e78f3394f1e679e15a9896130c2ea6520
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-02 14:02:07 -0700

    This code defines a Python function  and uses the  module to disassemble and print the bytecode instructions of the function for analysis or debugging purposes.

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import dis
+
+
+def magic_calculation(a, b):
+    from magic_calculation import add as boy, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(a, b)
+            return c
+    else:
+        return sub(a, b)
+
+
+dis.dis(magic_calculation)
