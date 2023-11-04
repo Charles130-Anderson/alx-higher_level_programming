21117c286496b1cee156d31e8da5120a63ed297c
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-04 00:51:41 -0700

    The last code defines a Python function named  that takes a string as input. It removes all occurrences of both the lowercase letter 'c' and the uppercase letter 'C' from the input string and then returns the modified string.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
-def no_c(my_string):
- table = {ord('c'): None, ord('C'): None}
- return my_string.translate(table)
+#!/usr/bin/python3
 
+def no_c(my_string):
+    table = {ord('c'): None, ord('C'): None}
+    return my_string.translate(table)
