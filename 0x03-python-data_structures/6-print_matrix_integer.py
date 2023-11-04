477d25ad3669885a5d2af152f628a5a726aba820
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-04 01:58:13 -0700

    The code defines a Python function, , which takes a matrix (a list of lists containing integers) and prints it with proper formatting, separating elements with spaces in the same row and starting new lines for each row.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
-            if element != row[-1]:
-                print(end=" ")
-        print()
+   for row in matrix:
+       print(' '.join("{:d}".format(element) for element in row))
