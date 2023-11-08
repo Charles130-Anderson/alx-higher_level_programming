f357abf382bfbae209486225d9da8eabbef20970
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-08 10:37:37 -0800

    This function works by using a dictionary comprehension to create a new dictionary from a_dictionary that excludes the keys with the value value. The function returns the new dictionary.

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
+    squared_matrix = [[x ** 2 for x in row] for row in matrix]
+    return squared_matrix
