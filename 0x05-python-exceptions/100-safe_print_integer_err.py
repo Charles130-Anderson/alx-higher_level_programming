7fcf91fa638aeca2a645d82fc8083d9a5142f9d2
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-20 11:06:16 -0800

    task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_integer_err(value):
-  try:
-      print("{:d}".format(value))
-      return True
-  except ValueError:
-      return "{} is not an integer".format(value)
-
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+    else:
+        return True
