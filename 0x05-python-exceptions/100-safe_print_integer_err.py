6167b736f9f0cba09a0745ae65e42546ab1b7ca1
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-20 06:34:20 -0800

    This code attempts to print an integer representation of a given value. If successful, it returns . If an error occurs (either  or ) and the value is not an integer or float, it prints an error message to the standard error and returns .

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        if not isinstance(value, (int, float)):
+            print("Exception: Unknown format code 'd' for object of type '{}'"
+                  .format(type(value).__name__), file=sys.stderr)
+        return False
