f25fc7581b4fb7159fbe2115bb5980794d905ad2
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-20 04:56:40 -0800

    This function creates a new list result of length list_length and initializes all elements to 0. It then iterates over the first list_length elements of my_list_1 and my_list_2.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 def list_division(my_list_1, my_list_2, list_length):
     result = [0] * list_length
 
     for i in range(list_length):
         try:
-            if not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
+            if (
+                not isinstance(my_list_1[i], (int, float)) or
+                not isinstance(my_list_2[i], (int, float))
+            ):
                 print("wrong type")
                 result[i] = 0
             elif my_list_2[i] == 0:
