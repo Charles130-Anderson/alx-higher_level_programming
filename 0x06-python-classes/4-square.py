1f13a73ff60efbf66460044baa3ebca0935c574f
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-21 07:28:58 -0800

    This code includes a private instance attribute __size, a getter @property for retrieving the size, and a setter @size.setter for setting the size with type and value validation. The instantiation with optional size and the area method are also implemented as specified.

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defining a class square"""
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """Initializes a new instance of the Square class."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
