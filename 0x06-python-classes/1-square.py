fd12d9de52f5ce65482fb615945594108025ce4b
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-21 06:20:51 -0800

    Initializing a new instance of a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Squaare module"""
+
 
 class Square:
-   def __init__(self, size):
-       self.__size = size
+    """
+    This class defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """Initializes a new instance of the Square class."""
+        self.__size = size
