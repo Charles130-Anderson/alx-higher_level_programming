a5ff1e356ebb695598c679cd61783ca1be6a4ca3
Charles130-Anderson <andersonoyoo034@gmail.com>
2023-11-21 07:08:18 -0800

    Defining class with private instance attributes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defining a class square"""
+
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """Initializes a new instance of the Square class."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
