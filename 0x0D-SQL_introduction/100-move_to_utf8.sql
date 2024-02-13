4cd8b09bca3368104ea8c28ccb90c72f4888a265
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-02-13 11:44:12 -0800

    SQL-introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,11 @@
--- Convert the entire hbtn_0c_0 database to utf8mb4 character set and utf8mb4_unicode_ci collation.
-USE 'hbtn_0c_0'
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- Task: Convert all tables in the hbtn_0c_0 database to utf8mb4 character set and utf8mb4_unicode_ci collation.
+
+-- Select the database to work with
+USE `hbtn_0c_0`;
+
+-- Get a list of all tables in the current database
+SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = DATABASE();
+
+-- Iterate over each table and convert its character set and collation
+-- Note: This part must be executed manually for each table returned by the SELECT statement above
+-- ALTER TABLE `table_name` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
