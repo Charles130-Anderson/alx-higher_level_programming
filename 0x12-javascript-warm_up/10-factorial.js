e0a62a6fc7abec28b587feff740e5c382d5e5722
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-11 13:47:22 -0700

    made semistandard compliant

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,10 +3,10 @@
 function factorial(n) {
   if (isNaN(n)) {
     return 1;
-  } else if (n === 0) {
+  } else if (n <= 1) {
     return 1;
   } else {
-    return (n * factorial(n - 1));
+    return n * factorial(n - 1);
   }
 }
 
