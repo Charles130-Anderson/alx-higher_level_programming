fc27dcc34c0217b437af1bf514d2708cd2e69243
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-11 12:59:08 -0700

    made semistandard compliant

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,18 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  return +a + +b;
+    return a + b;
 }
 
-const num1 = process.argv[2];
-const num2 = process.argv[3];
-
-console.log(add(num1, num2));
+const args = process.argv.slice(2);
+if (args.length < 2) {
+    console.log("NaN");
+} else {
+    const num1 = parseInt(args[0], 10);
+    const num2 = parseInt(args[1], 10);
+    if (isNaN(num1) || isNaN(num2)) {
+        console.log("NaN");
+    } else {
+        console.log(add(num1, num2));
+    }
+}
