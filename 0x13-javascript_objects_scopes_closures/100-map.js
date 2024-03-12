adbb85ad2dc950742dfcd9ffe842655d3048722c
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-12 04:39:49 -0700

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,10 +2,11 @@
 
 const { list } = require('./100-data');
 
-const newList = [];
-for (let i = 0; i < list.length; i++) {
-  newList.push(list[i] * i);
+function multiplyByIndex(value, index) {
+  return value * index;
 }
+
+const newList = list.map(multiplyByIndex);
 
 console.log(list);
 console.log(newList);
