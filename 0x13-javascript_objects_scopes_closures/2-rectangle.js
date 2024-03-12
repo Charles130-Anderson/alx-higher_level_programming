bf9aadecf7ac09c26aa696f72304f0a94dd625f6
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-12 02:43:14 -0700

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,12 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
       return {};
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
