51fed4087ecfedfd1de5bf25c48c240d3ef9f400
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-27 12:23:09 -0700

    Python - Network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # For checking if the argument is not equal to 2
-curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
-
+curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
