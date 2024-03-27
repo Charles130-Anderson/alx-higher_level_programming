66af9e44436179f217f18583303654cb927adcf9
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-27 11:55:40 -0700

    Python - Network

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Module: 6-peak
+
+Contains a function to find a peak in a list of unsorted integers.
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): List of unsorted integers.
+
+    Returns:
+        int: A peak element.
+
+    Complexity:
+        O(log(n)), where n is the number of elements in the list.
+    """
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
