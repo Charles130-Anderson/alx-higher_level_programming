fa937e9158bf4f7e2a332ad28d2fdacdb049fa55
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-03-27 12:13:43 -0700

    Python - Network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
+#for checking if the argument is not equal to 2
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
 
-# Sends a JSON POST request to a URL and displays the body of the response
-
-# Check if the file exists
-if [ ! -f "$2" ]; then
-    echo "File '$2' does not exist."
-    exit 1
-fi
-
-# Check if the file contains valid JSON
-if ! jq . "$2" >/dev/null 2>&1; then
-    echo "Not a valid JSON file."
-    exit 1
-fi
-
-# Send the JSON POST request and display the response body
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
