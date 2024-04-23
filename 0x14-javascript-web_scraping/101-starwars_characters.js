a7476a1bf78f581ad91a38ef60b0375eb7b11c95
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-04-23 08:35:37 -0700

    javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,38 +3,29 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(apiUrl, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const movieData = JSON.parse(body);
-    const charactersUrls = movieData.characters;
-    
-    // Function to fetch character data
-    const fetchCharacterData = (charUrl) => {
-      return new Promise((resolve, reject) => {
-        request(charUrl, function (charError, charResponse, charBody) {
-          if (!charError && charResponse.statusCode === 200) {
-            const characterData = JSON.parse(charBody);
-            resolve(characterData.name);
-          } else {
-            reject(charError || new Error('Failed to fetch character data'));
-          }
-        });
-      });
-    };
-
-    // Fetch character data for each character URL
-    Promise.all(charactersUrls.map(fetchCharacterData))
-      .then((characters) => {
-        characters.forEach((character) => {
-          console.log(character);
-        });
-      })
-      .catch((err) => {
-        console.error('Error:', err);
-      });
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   } else {
-    console.error('Error fetching movie data:', error);
+    console.error('Error:', error);
   }
 });
+
+function printCharacters(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  request(characters[index], function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+      printCharacters(characters, index + 1);
+    } else {
+      console.error('Error:', error);
+    }
+  });
+}
